18dfb165c2ba17d8a8f03f07863d7c54447b1c6f
SonAIengine <sonsj97@gmail.com>
2025-07-17 16:58:31 +0900

    feat: Update requirements and refactor RAG controller to integrate new embedding services

--- src/embeddings/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-임베딩 모듈
-다양한 임베딩 제공자를 지원하는 통합 임베딩 시스템
-"""
-
-from .embedding_factory import EmbeddingFactory
-from .base_embedding import BaseEmbedding
-
-__all__ = ["EmbeddingFactory", "BaseEmbedding"] 
