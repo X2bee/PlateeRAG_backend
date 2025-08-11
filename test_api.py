1d509c88b75fc911cc54a0be0a1a9550913f35bd
CocoRoF <gkfua00@gmail.com>
2025-08-11 01:12:34 +0000

    feat: Enhance InputStringNode to support ArgsSchema validation and improve error handling; update test API with new workflow parameters

--- test_api.py
+++ test_api.py
@@ -1,25 +1,23 @@
 import requests
 
-API_URL = "http://192.168.219.101:8000/api/workflow/deploy/execute/based_id"
+API_URL = "http://localhost:8000/api/workflow/deploy/execute/based_id"
 
 def query(payload):
     response = requests.post(API_URL, json=payload)
     return response.json()
 
 output = query({
-    "workflow_name": "Workflow_vllm_hf",
-    "workflow_id": "workflow_195af1fd3b72f102c426f13c1e9375cf819dcb10",
-    "input_data": "hf에서 데이터 셋의 정보를 가져와 줘.",
+    "workflow_name": "Workflow-test",
+    "workflow_id": "workflow_f8529d8932d17d20dcbc96627819bea321aa89a3",
+    "input_data": "안녕하세요",
     "interaction_id": "default",
     "selected_collection": "string",
-    "additional_params": {
-        "api_loader/APICallingTool-1754720502413": {
-            "dataset_path": "str",
-            "hugging_face_user_id": "str",
-            "hugging_face_token": "str"
-        }
-    },
-    "user_id": "1"
+    # "additional_params": {
+    #     "api_loader/APICallingTool-1754637013411": {
+    #         "return_dict": "bool"
+    #     }
+    # },
+    "user_id": "17"
 })
 
 print(output)
